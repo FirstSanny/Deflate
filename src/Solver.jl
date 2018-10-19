d340e8d11f1d5a710986c3a972f7a4a80bf9dd55
FirstSanny <FirstSanny@3582537.no-reply.drupal.org>
2018-10-19 12:23:22 +0200

    inv mit toleranz nutzen, falls inv fehlschlägt

--- src/Solver.jl
+++ src/Solver.jl
@@ -10,7 +10,12 @@ using IterativeSolvers
 
         P = transpose(R)
 
-        Q = P*inv(R*A*P)*R
+        try
+            Q = P*inv(R*A*P)*R
+        catch
+            println("pinv was used")
+            Q = P*pinv(R*A*P)*R
+        end
 
         if(useM3)
             PN = inv(Diagonal(A))*(I - A * Q) + Q
@@ -19,6 +24,6 @@ using IterativeSolvers
         end
 
 
-        IterativeSolvers.gmres(A, b; tol = 1/100000000, Pl = LinearAlgebra.lu(PN), log = true)   
+        IterativeSolvers.gmres(A, b; tol = 1/100000000, Pl = LinearAlgebra.lu(PN), log = true)
     end
 end
